7dab8dd4cadcd27bcf313953255b5560d82bfe24
rheikal <rheikal@hkl-telecom.com>
2022-03-04 16:20:24 +0800

    Add files via upload

---
+++ psql_install.sh
@@ -0,0 +1,35 @@
+echo "cleaning up PostgreSQL"
+sudo systemctl start postgresql
+sudo yum remove -y postgresql-server postgresql-contrib
+rm -rf /var/lib/pgsql/data
+echo "installing PostgreSQL"
+sudo yum install -y postgresql-server postgresql-contrib
+echo "Running initdb"
+sudo postgresql-setup initdb
+cp -rp /home/ec2-user/psql/pg_hba.conf /var/lib/pgsql/data
+echo "Starting PostgreSQL"
+sudo systemctl start postgresql
+sudo systemctl enable postgresql
+
+# This block adds the environment variables for PostgreSQL to /etc/profile in order to set them for all users.
+#echo "Writing PostgreSQL environment variables to /etc/profile"
+#cat << EOL | sudo tee -a /etc/profile
+# PostgreSQL Environment Variables
+#LD_LIBRARY_PATH=/postgres/lib
+#export LD_LIBRARY_PATH
+#PATH=/postgres/bin:$PATH
+#export PATH
+#EOL
+
+echo "Wait for PostgreSQL to finish starting up..."
+sleep 5
+
+# The startup.sql script is ran to create the user, database, and populate the database.
+echo "Running script"
+helloscript='/home/ec2-user/psql/startup.sql'
+psql -U postgres -f $helloscript
+
+# hello_postgres is queried
+
+echo "Querying the newly created table in the newly created database."
+psql -c 'select * from hello;' -U psqluser hello_postgres;
